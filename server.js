bbcc42e69f3992b002cd95f541992848d42c9413
Fabio <fabiolombardi@Fabios-MacBook-Pro.local>
2015-03-14 17:41:48 +0100

    Creating the angular Application

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var stylus = require('stylus');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+if (env === 'development') {
+    app.set('view engine', 'jade');
+    app.set('views', __dirname + '/server/views');
+    app.set('view engine', 'jade');
+    app.set('views', __dirname + '/server/views');
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(stylus.middleware (
+        {
+            src: __dirname + '/public',
+            compile: compile
+        }
+    ));
+    app.use(express.static(__dirname + '/public'));
+}
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function (req, res) {
+    res.render('index')
+});
+
+var port = 3030;
+
+var server = app.listen(port, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Example app listening at http://%s:%s', host, port)
+
+})
