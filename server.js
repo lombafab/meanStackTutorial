7d572203353a5b6a1c538651d1b45739a8287d6e
Fabio <fabiolombardi@Fabios-MacBook-Pro.local>
2015-03-14 18:15:43 +0100

    Chapter 2 Creating a walking scheleton finished

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (env === 'development') {
     ));
     app.use(express.static(__dirname + '/public'));
 }
-
+// MongoDB connect
 mongoose.connect('mongodb://localhost/multivision');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error..."));
@@ -35,12 +35,22 @@ db.once('open', function callback() {
     console.log('multivision db opened');
 });
 
+//Creating a message schema
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+   mongoMessage = messageDoc.message;
+});
+
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function (req, res) {
-    res.render('index')
+    res.render('index', {
+       mongoMessage: mongoMessage
+    });
 });
 
 var port = 3030;
